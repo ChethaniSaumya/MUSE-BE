8f63797d0423b247ec7bfc01d82e535898406809
Chethani Saumya <105449384+ChethaniSaumya@users.noreply.github.com>
2025-06-11 00:10:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -531,7 +531,7 @@ app.get('/api/download-certificate/:email', cors(corsOptions), async (req, res) 
     }
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
